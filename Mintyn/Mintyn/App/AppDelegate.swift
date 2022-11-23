369789f698dd12486ea3bdadbd91f3cadc790139
Omoyeni <babatundeabike3@gmail.com>
2022-11-23 23:15:00 +0100

    appdelegate_scenedelegate_updated_to_programatically_launch_viewcontrollers_with_

---
+++ Mintyn/Mintyn/App/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  Mintyn
+//
+//  Created by Sharon Omoyeni Babatunde on 23/11/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        setUpRootViewController(window: window)
+        return true
+    }
+
+    func setUpRootViewController(window: UIWindow) {
+        let loginViewController = LoginViewController(nibName: "LoginViewController", bundle: nil)
+        let navController = UINavigationController(rootViewController: loginViewController)
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+    
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+
+
+}
+
---
+++ Mintyn/Mintyn/App/SceneDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  SceneDelegate.swift
+//  Mintyn
+//
+//  Created by Sharon Omoyeni Babatunde on 23/11/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let loginViewController = LoginViewController(nibName: "LoginViewController", bundle: nil)
+        let navController = UINavigationController(rootViewController: loginViewController)
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+    
+    
+}
+
