b452304d70f2e5bb5080b2f6826fd997117e3bca
Omoyeni <babatundeabike3@gmail.com>
2022-11-23 23:13:12 +0100

    Create LoginDisplayModel.swift

---
+++ Mintyn/Mintyn/View/LoginPage/Model/LoginDisplayModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginDisplayModel.swift
+//  Mintyn
+//
+//  Created by Sharon Omoyeni Babatunde on 23/11/2022.
+//
+
+import Foundation
+
+struct LoginDisplayModel {
+    let image: String
+    let label: String
+    let labelStatus: String?
+}
