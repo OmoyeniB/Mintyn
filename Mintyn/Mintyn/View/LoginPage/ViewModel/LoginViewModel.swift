c88156e33c345fbf7d0af9097a84381b2d6f90c7
Omoyeni <babatundeabike3@gmail.com>
2022-11-24 22:17:03 +0100

    Update LoginViewModel.swift

--- Mintyn/Mintyn/View/LoginPage/ViewModel/LoginViewModel.swift
+++ Mintyn/Mintyn/View/LoginPage/ViewModel/LoginViewModel.swift
@@ -5,14 +5,24 @@
 //  Created by Sharon Omoyeni Babatunde on 23/11/2022.
 //
 
-import Foundation
+import UIKit
 
 protocol LoginViewModelProtocol {
     func createLoginModel() -> [LoginDisplayModel]
-  
+    
 }
 
 final class LoginViewModel: LoginViewModelProtocol {
+    
+    var saveThemeState = false {
+        didSet {
+            themeState()
+        }
+    }
+    var darkLightThemeControl = false
+    func themeState() {
+        UserDefaults.standard.set(saveThemeState, forKey: "themeState")
+    }
     
     func createLoginModel() -> [LoginDisplayModel] {
         return [LoginDisplayModel(image: "shield", label: "Insurance", labelStatus: "Coming soon..."),
@@ -21,5 +31,56 @@ final class LoginViewModel: LoginViewModelProtocol {
                 LoginDisplayModel(image: "support", label: "Contact Support", labelStatus: nil)
                 
         ]
+    }
+    
+    func invalidPhoneNumber(_ phoneNumber: String) -> String? {
+        let set = CharacterSet(charactersIn: phoneNumber)
+        if !CharacterSet.decimalDigits.isSuperset(of: set) {
+            return "Phone number must contain only digits"
+        }
+        if phoneNumber.count != 10 || phoneNumber.count >= 11 {
+            return "Phone number must be more than 10 digit and less than 11 digits"
+        }
+        return nil
+    }
+    
+    func invalidPassword(_ password: String) -> String? {
+        if password.count < 8 {
+            return "Password must be at least 8 characters"
+        }
+        if containsDigit(password) {
+            return "Password must contain at least 1 digit"
+        }
+        if containsLowerCase(password) {
+            return "Password must contain at least 1 lowercase character"
+        }
+        if containsUpperCase(password) {
+            return "Password must contain at least 1 uppercase character"
+        }
+        return nil
+    }
+    
+    func containsDigit(_ value: String) -> Bool {
+        let reqularExpression = ".*[0-9]+.*"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
+        return !predicate.evaluate(with: value)
+    }
+    
+    func containsLowerCase(_ value: String) -> Bool {
+        let reqularExpression = ".*[a-z]+.*"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
+        return !predicate.evaluate(with: value)
+    }
+    
+    func containsUpperCase(_ value: String) -> Bool {
+        let reqularExpression = ".*[A-Z]+.*"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
+        return !predicate.evaluate(with: value)
+    }
+    
+    func resetForm(_ passwordTextField: UITextField, _ phoneNumberTextField: UITextField, _ errorMessage: UILabel) {
+        passwordTextField.text = ""
+        phoneNumberTextField.text = ""
+        errorMessage.isHidden = true
     }
 }
