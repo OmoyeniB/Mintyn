6b7a4c60ebabeca2a0b03a65549e5d5c476eb19d
Omoyeni <babatundeabike3@gmail.com>
2022-11-24 22:08:56 +0100

    Create HomePageProductModel.swift

---
+++ Mintyn/Mintyn/View/HomePage/Model/HomePageProductModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomePageProductModel.swift
+//  Mintyn
+//
+//  Created by Sharon Omoyeni Babatunde on 24/11/2022.
+//
+
+import Foundation
+
+struct HomePageProductModel {
+    let image: String
+}
