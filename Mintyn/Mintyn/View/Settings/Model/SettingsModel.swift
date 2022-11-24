3a618653bc3c6ab0a159dc8b7b1f5fd2c6a438e0
Omoyeni <babatundeabike3@gmail.com>
2022-11-24 22:17:31 +0100

    Create SettingsModel.swift

---
+++ Mintyn/Mintyn/View/Settings/Model/SettingsModel.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsModel.swift
+//  Mintyn
+//
+//  Created by Sharon Omoyeni Babatunde on 24/11/2022.
+//
+
+import Foundation
+
+struct SettingsModel {
+    let image: String
+    let label: String
+}
+
+struct Legal_System_Model {
+    let label: String
+}
