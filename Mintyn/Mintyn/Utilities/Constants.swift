8e5a31a6b960daeb1814b613bc8838729cd5e860
Omoyeni <babatundeabike3@gmail.com>
2022-11-24 22:08:03 +0100

    create_constants_to_reference_constant_strings

---
+++ Mintyn/Mintyn/Utilities/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  Mintyn
+//
+//  Created by Sharon Omoyeni Babatunde on 23/11/2022.
+//
+
+import UIKit
+
+enum Constants {
+    enum Strings {
+        static let viewcontrollerLabel = "Please check *Home* and *Settings* Tab"
+    }
+    enum Colors {
+        static let blackColor = UIColor(named: "BlackColor")
+        static let goldColor = UIColor(named: "GoldColor")
+        static let greyColor = UIColor(named: "GreyColor")
+        static let lightBlack = UIColor(named: "LightBlackColor")
+        static let whiteColor = UIColor(named: "WhiteColor")
+        static let textColor = UIColor(named: "TextColor")
+        static let lighGreyColor = UIColor(named: "LightGreyColor")
+    }
+}
