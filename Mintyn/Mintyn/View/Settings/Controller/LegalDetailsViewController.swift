4952ddb1c44ef6a3419c9d658c1a070c46baa450
Omoyeni <babatundeabike3@gmail.com>
2022-11-24 22:17:22 +0100

    Create LegalDetailsViewController.swift

---
+++ Mintyn/Mintyn/View/Settings/Controller/LegalDetailsViewController.swift
@@ -0,0 +1,81 @@
+//
+//  LegalDetailsViewController.swift
+//  Mintyn
+//
+//  Created by Sharon Omoyeni Babatunde on 24/11/2022.
+//
+
+import UIKit
+
+class LegalDetailsViewController: UIViewController {
+    var label: String?
+    let viewModel = SettingsViewModel()
+    let tableView = UITableView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        view.addSubview(tableView)
+        configureTableView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(SettingsLegalCell.self, forCellReuseIdentifier: SettingsLegalCell.identifier)
+        tableView.backgroundColor = Constants.Colors.lightBlack
+    }
+    
+    
+}
+
+extension LegalDetailsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.showLegalModelData().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch label {
+        case "Legal":
+            if let cell = tableView.dequeueReusableCell(withIdentifier: SettingsLegalCell.identifier, for: indexPath) as? SettingsLegalCell, let label {
+                cell.setUpCell(model: viewModel.showLegalModelData()[indexPath.row], checklabel: label)
+                return cell
+            }
+        case "System":
+            if let cell = tableView.dequeueReusableCell(withIdentifier: SettingsLegalCell.identifier, for: indexPath) as? SettingsLegalCell, let label {
+                cell.setUpCell(model: viewModel.showSettingData()[indexPath.row], checklabel: label)
+//                cell.pointerImage.isHidden = true
+                cell.pointerImage.setImage(UIImage(systemName: "checkmark"), for: .normal)
+                return cell
+            }
+        default:
+            break
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        50
+    }
+}
+
+extension LegalDetailsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+        if let label {
+            viewModel.configureAppearance(label: label, indexPath: indexPath)
+        }
+    }
+}
+
+
+
+
+
