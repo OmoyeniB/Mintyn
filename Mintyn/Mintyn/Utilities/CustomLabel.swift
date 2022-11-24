dc60f0f8382552ba1febb02c254f361469b64d33
Omoyeni <babatundeabike3@gmail.com>
2022-11-24 22:01:58 +0100

    create_custom_label

---
+++ Mintyn/Mintyn/Utilities/CustomLabel.swift
@@ -0,0 +1,31 @@
+//
+//  CustomLabel.swift
+//  Mintyn
+//
+//  Created by Sharon Omoyeni Babatunde on 24/11/2022.
+//
+
+import UIKit
+
+public class MyCustomLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.text = "Please check *Home* and *Settings* Tab"
+        self.textAlignment = .center
+        self.numberOfLines = 0
+        self.textColor = Constants.Colors.whiteColor
+        self.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+    }
+}
+
