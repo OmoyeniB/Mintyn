3345d964b028e6cf47096c647aa65c1dd4b79244
Omoyeni <babatundeabike3@gmail.com>
2022-11-25 09:55:06 +0100

    unit_test_for_login_validation

--- Mintyn/MintynTests/MintynTests.swift
+++ Mintyn/MintynTests/MintynTests.swift
@@ -6,26 +6,63 @@
 //
 
 import XCTest
+@testable import Mintyn
 
 final class MintynTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private var validator: CreateValidator!
+    
+    override func setUp() {
+        validator = CreateValidator()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        validator = nil
     }
-
-    func testExample() throws {
-       
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_with_empty_password_error_thrown() {
+        let password = ""
+        XCTAssertThrowsError(try validator.validatePassword(password), "Error for an empty password")
+        do {
+            _ = try validator.validatePassword(password)
+        } catch {
+            guard let passwordError = error as? CreateValidator.CreateValidatorError else {
+                XCTFail("Not the error type expected, ...expecting a create validator error")
+                return
+            }
+            XCTAssertEqual(passwordError, CreateValidator.CreateValidatorError.invalidPassword, "Expecting an error of invalid password")
         }
     }
-
+    
+    func test_with_empty_phoneNumber_error_thrown() {
+        let phoneNumber = ""
+        XCTAssertThrowsError(try validator.validatePhoneNumber(phoneNumber), "Error for an empty phoneNumber")
+        do {
+            _ = try validator.validatePhoneNumber(phoneNumber)
+        } catch {
+            guard let phoneNumberError = error as? CreateValidator.CreateValidatorError else {
+                XCTFail("Not the error type expected, ...expecting a create validator error")
+                return
+            }
+            XCTAssertEqual(phoneNumberError, CreateValidator.CreateValidatorError.invalidPhoneNumber, "Expecting an error of invalid phoneNumber")
+        }
+    }
+    
+    func test_with_valid_phoneNumber_error_not_thrown() {
+        let phoneNumber = "070123456789"
+        do {
+            _ = try validator.validatePhoneNumber(phoneNumber)
+        } catch {
+            XCTFail("No errors should be thrown, since the person should be a valid object")
+        }
+    }
+    
+    func test_with_valid_password_error_not_thrown(){
+        let password = "Omoyeni"
+        do {
+            _ = try validator.validatePassword(password)
+        } catch {
+            XCTFail("No errors should be thrown, since the person should be a valid object")
+        }
+    }
+    
 }
